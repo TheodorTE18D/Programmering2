2835fc06c11a14db1c13abaa2d3de1b0904a040c
TheodorTE18D <54800526+TheodorTE18D@users.noreply.github.com>
2020-08-30 20:24:12 +0200

    Variabler och Datatyper i C#

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,34 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Niklas!");
+            Console.WriteLine("What is your name?");
+            String name = Console.ReadLine();
+
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Are you alive?");
+            bool alive = false;
+            if(Console.ReadLine() == "yes")
+            {
+                alive = true;
+            }
+                
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+
+            if (alive)
+            {
+                Console.WriteLine("Lucky you!");
+            }
+            else
+            {
+                Console.WriteLine("RIP");
+            }
+
+            Console.ReadLine();
+
+  
         }
     }
 }
