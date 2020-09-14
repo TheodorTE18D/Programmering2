4791111e2a5acefda873fb89977a08432529fd8d
TheodorTE18D <54800526+TheodorTE18D@users.noreply.github.com>
2020-09-14 17:20:42 +0200

    1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nmr1 = 20;
+            int nmr2 = 34;
+
+            while (true) { 
+            Console.WriteLine("Hello! Which number is greater than the other?");
+            Console.WriteLine(nmr1 + " or " + nmr2);
+            
+
+                if (answer != null)
+                {
+                    break;
+                }
+            }
+            bool correct = false;
+            if (answer == nmr2)
+            {
+                Console.WriteLine("Correct!");
+                correct = true;
+            }
+            else if(answer == nmr1){
+                Console.WriteLine("Wrong!");
+            }
+            else
+            {
+                Console.WriteLine("Write " + nmr1 + " or " + nmr2);
+            }
+
+            switch (correct){
+
+                case true:
+
+                    break;
+                    ;
+
+                case false:
+                    Console.WriteLine("Try again!");
+                    break;
+                    ;
+            }
+        }
+    }
+}
---
+++ HelloWorld/Metoder/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Metoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! Write five different names");
+            string[] names = {};
+    
+            Console.WriteLine(names);
+        }
+    }
+}
